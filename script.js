46deead460aaa92371e2928c2a0e39874c1131f0
Magdalena Zych <magdalena.zych@interia.pl>
2023-08-15 20:08:39 +0200

    js refactoring

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-let amountElement = document.querySelector(".js-amount")
-let formElement = document.querySelector(".js-form")
-let currencyElement = document.querySelector(".js-currency")
-let sumElement = document.querySelector(".js-sum")
+{
+    const calculateResult = (amount, currency) => {
+        const currencyUSD = 4.11;
+        const currencyEUR = 4.46;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+        switch (currency) {
+            case "USD":
+                return amount / currencyUSD;
 
-    let currency = currencyElement.value;
-    let amount = amountElement.value;
-    let sum = sumElement.value;
-    let currencyUSD = 4.11;
-    let currencyEUR = 4.46;
+            case "EUR":
+                return amount / currencyEUR;
+        }
+    };
 
-    switch (currency) {
-        case "USD":
-            sum = amount * currencyUSD;
-            break;
-        case "EUR":
-            sum = amount * currencyEUR;
-            break;
+    const updateResultText = (amount, sum, currency) => {
+        const sumElement = document.querySelector(".js-sum")
+        sumElement.innerHTML = `${amount.toFixed(2)} PLN = ${sum.toFixed(2)} ${currency}`;
     }
-    
-    sumElement.innerText = `${sum.toFixed(2)} ${currency}`;
 
-});
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount")
+        const currencyElement = document.querySelector(".js-currency")
+
+
+        const amount = +amountElement.value
+        const currency = currencyElement.value
+
+        const sum = calculateResult(amount, currency);
+
+        updateResultText(amount, sum, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form")
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
